b55e8b291a5cbaf5683f1644d59d6e72cdd8eaa2
xCuri0 <zkqri0@gmail.com>
2021-06-08 13:40:25 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace KeepAwake
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AwakePrograms = new System.Windows.Forms.ListBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Remove = new System.Windows.Forms.Button();
+            this.Startup = new System.Windows.Forms.CheckBox();
+            this.Active = new System.Windows.Forms.CheckBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // AwakePrograms
+            // 
+            this.AwakePrograms.FormattingEnabled = true;
+            this.AwakePrograms.HorizontalScrollbar = true;
+            this.AwakePrograms.ItemHeight = 15;
+            this.AwakePrograms.Location = new System.Drawing.Point(12, 12);
+            this.AwakePrograms.Name = "AwakePrograms";
+            this.AwakePrograms.Size = new System.Drawing.Size(243, 214);
+            this.AwakePrograms.TabIndex = 0;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(261, 12);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Remove
+            // 
+            this.Remove.Location = new System.Drawing.Point(261, 41);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(75, 23);
+            this.Remove.TabIndex = 2;
+            this.Remove.Text = "Remove";
+            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // Startup
+            // 
+            this.Startup.AutoSize = true;
+            this.Startup.Location = new System.Drawing.Point(261, 70);
+            this.Startup.Name = "Startup";
+            this.Startup.Size = new System.Drawing.Size(64, 19);
+            this.Startup.TabIndex = 3;
+            this.Startup.Text = "Startup";
+            this.Startup.UseVisualStyleBackColor = true;
+            this.Startup.CheckedChanged += new System.EventHandler(this.Startup_CheckedChanged);
+            // 
+            // Active
+            // 
+            this.Active.AutoSize = true;
+            this.Active.Enabled = false;
+            this.Active.Location = new System.Drawing.Point(261, 95);
+            this.Active.Name = "Active";
+            this.Active.Size = new System.Drawing.Size(59, 19);
+            this.Active.TabIndex = 4;
+            this.Active.Text = "Active";
+            this.Active.UseVisualStyleBackColor = true;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "KeepAwake";
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 233);
+            this.Controls.Add(this.Active);
+            this.Controls.Add(this.Startup);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.AwakePrograms);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "KeepAwake";
+            this.Load += new System.EventHandler(this.FormLoad);
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox AwakePrograms;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.CheckBox Startup;
+        private System.Windows.Forms.CheckBox Active;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeepAwake
+{
+    public partial class Form1 : Form
+    {
+
+        RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern ExecutionState SetThreadExecutionState(ExecutionState esFlags);
+
+        [FlagsAttribute]
+        private enum ExecutionState : uint
+        {
+            EsAwaymodeRequired = 0x00000040,
+            EsContinuous = 0x80000000,
+            EsDisplayRequired = 0x00000002,
+            EsSystemRequired = 0x00000001
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1 && args[1] == "-tray") {
+                Hide();
+                ShowInTaskbar = false;
+                WindowState = FormWindowState.Minimized;
+                notifyIcon.Visible = true;
+            }
+
+            try
+            {
+                foreach (string item in File.ReadAllLines(Path.GetDirectoryName(Application.ExecutablePath) + "\\KeepAwake.dat"))
+                {
+                    AwakePrograms.Items.Add(item);
+                }
+            }
+            catch (FileNotFoundException)
+            { }
+        }
+
+        private void MonitorLoop()
+        {
+            Process[] processList;
+
+            try
+            {
+                while (true)
+                {
+                    bool found = false;
+                    processList = Process.GetProcesses();
+                    foreach (var process in processList)
+                    {
+                        try
+                        {
+                            if (AwakePrograms.Items.IndexOf(process.MainModule.FileName.ToLower()) != -1)
+                            {
+                                found = true;
+                                // Prevent sleep
+                                SetThreadExecutionState(ExecutionState.EsContinuous | ExecutionState.EsSystemRequired);
+                                if (!Active.Checked)
+                                    Invoke(new Action(() =>
+                                    {
+                                        Active.Checked = true;
+                                        Active.Update();
+                                    }));
+                                }
+                        }
+                        catch (Exception ex)
+                        {
+                            if (ex is Win32Exception || ex is InvalidOperationException)
+                                continue;
+                            throw;
+                        }
+                        System.Threading.Thread.Sleep(100);
+                    }
+                    if (!found)
+                    {
+                        // Allow sleep
+                        SetThreadExecutionState(ExecutionState.EsContinuous);
+                        Invoke(new Action(() =>
+                        {
+                            Active.Checked = false;
+                            Active.Update();
+                        }));
+                    }
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                return;
+            }
+        }
+        private void FormLoad(object sender, EventArgs e)
+        {
+            notifyIcon.Icon = SystemIcons.Application;
+
+            Startup.Checked = rk.GetValue("KeepAwake") != null;
+            Startup.Update();
+
+            Task.Run((Action)MonitorLoop);
+        }
+
+        private void SaveConfig()
+        {
+            List<string> wakePrograms = new List<string>();
+            foreach (var program in AwakePrograms.Items)
+                wakePrograms.Add(program.ToString());
+
+            File.WriteAllLines(Path.GetDirectoryName(Application.ExecutablePath) + "\\KeepAwake.dat", wakePrograms.ToArray());
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Executeable|*.exe";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (AwakePrograms.Items.IndexOf(openFileDialog.FileName.ToLower()) != -1)
+                        return;
+
+                    AwakePrograms.Items.Add(openFileDialog.FileName.ToLower());
+                    SaveConfig();
+                }
+            }
+        }
+
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            AwakePrograms.Items.RemoveAt(AwakePrograms.SelectedIndex);
+            SaveConfig();
+        }
+
+        private void Startup_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Startup.Checked)
+            {
+                rk.SetValue("KeepAwake", Application.ExecutablePath + " -tray");
+            }
+            else
+            {
+                rk.DeleteValue("KeepAwake", false);
+            }
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                ShowInTaskbar = false;
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            ShowInTaskbar = true;
+            notifyIcon.Visible = false;
+        }
+    }
+}
